755a7acbfc51765bc5b60bfa01d2e566248b60dc
TimNekk <herew26@gmail.com>
2022-12-31 01:31:36 +0300

    feat: add file writing exception

---
+++ src/timnekk/exceptions/FileWritingException.java
@@ -0,0 +1,7 @@
+package timnekk.exceptions;
+
+public class FileWritingException extends Exception {
+    public FileWritingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
