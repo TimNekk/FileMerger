bead22f178ca843eccaf2756083f9be4419f1c57
TimNekk <herew26@gmail.com>
2022-12-31 00:32:55 +0300

    feat: add optional and custom exception

--- src/timnekk/models/FileDependenciesFinder.java
+++ src/timnekk/models/FileDependenciesFinder.java
@@ -1,8 +1,11 @@
-package timnekk;
+package timnekk.models;
+
+import timnekk.exceptions.CanNotGetDependenciesException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -20,26 +23,24 @@ public final class FileDependenciesFinder implements DependenciesFinder<File> {
      *
      * @param file File to find dependencies of
      * @return Set of files that the file depends on
-     * @throws FileNotFoundException If the file is not found
+     * @throws CanNotGetDependenciesException if the file can not be read
      */
-    public Set<File> findDependencies(File file) {
+    public Set<File> findDependencies(File file) throws CanNotGetDependenciesException {
         Set<File> dependencies = new HashSet<>();
 
         Scanner scanner;
         try {
             scanner = new Scanner(file);
         } catch (FileNotFoundException e) {
-            return null;
-            // TODO: Handle exception
+            throw new CanNotGetDependenciesException(
+                    "Can not read file to find dependencies: " + file.getAbsolutePath(), e);
         }
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
 
-            File dependency = findDependentFile(line);
-            if (dependency != null) {
-                dependencies.add(dependency);
-            }
+            Optional<File> dependency = findDependentFile(line);
+            dependency.ifPresent(dependencies::add);
         }
 
         return dependencies;
@@ -51,15 +52,15 @@ public final class FileDependenciesFinder implements DependenciesFinder<File> {
      * @param line Line of code
      * @return File if a dependency is found, null otherwise
      */
-    private File findDependentFile(String line) {
+    private Optional<File> findDependentFile(String line) {
         Pattern pattern = Pattern.compile("require ‘(.+)’");
         Matcher matcher = pattern.matcher(line);
 
         if (matcher.find()) {
             String dependencyPath = matcher.group(1);
-            return new File(root, dependencyPath);
+            return Optional.of(new File(root, dependencyPath));
         }
 
-        return null;
+        return Optional.empty();
     }
 }
