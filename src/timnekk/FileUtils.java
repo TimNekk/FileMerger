69e56ded65d046717483e4da9509217b534e0602
TimNekk <herew26@gmail.com>
2022-12-31 00:33:36 +0300

    feat: add printFile method

--- src/timnekk/FileUtils.java
+++ src/timnekk/FileUtils.java
@@ -1,8 +1,11 @@
 package timnekk;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Scanner;
 import java.util.Set;
 
 public final class FileUtils {
@@ -25,5 +28,18 @@ public final class FileUtils {
         }
 
         return files;
+    }
+
+    /**
+     * Prints contents of a file
+     *
+     * @param file File to print
+     * @throws FileNotFoundException if the file can not be read
+     */
+    public static void printFile(File file, PrintStream printStream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            printStream.println(scanner.nextLine());
+        }
     }
 }
