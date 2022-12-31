02e8e6b1151562cfc77df92ef21b47a4d05f588e
TimNekk <herew26@gmail.com>
2022-12-31 16:24:35 +0300

    feat: use try with resources

--- src/timnekk/Main.java
+++ src/timnekk/Main.java
@@ -2,7 +2,8 @@ package timnekk;
 
 public final class Main {
     public static void main(String[] args) {
-        Application application = new Application(System.in, System.out);
-        application.run();
+        try (Application application = new Application(System.in, System.out)) {
+            application.run();
+        }
     }
 }
